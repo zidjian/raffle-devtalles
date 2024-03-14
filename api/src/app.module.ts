78783d9df7ad8fb7195a74f263f2f43c2f478c59
Camilo <camilo.reyes_granada@uao.edu.co>
2024-03-14 18:40:47 -0500

    add: typeorm configs

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CommonModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CommonModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
