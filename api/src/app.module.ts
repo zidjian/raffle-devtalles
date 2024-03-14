8e729d18c552521f21b8130eeac6cffd4b466749
Camilo <camilo.reyes_granada@uao.edu.co>
2024-03-14 11:20:33 -0500

    add: imports ConfigModule for env variables,  Common Module and AuthModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), CommonModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
