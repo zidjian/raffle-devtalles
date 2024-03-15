9ebaef03cbc6d62f57815eb65cb3d23722bb390e
Camilo <camilo.reyes_granada@uao.edu.co>
2024-03-15 09:43:20 -0500

    add: Validation Pipes

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT);
