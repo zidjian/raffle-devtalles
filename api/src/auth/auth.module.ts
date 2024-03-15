95ab5028172a40d8c6ec062273baf07524cdb820
Camilo <camilo.reyes_granada@uao.edu.co>
2024-03-14 19:50:34 -0500

    add: TypeOrm Module for User

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 import { CommonModule } from 'src/common/common.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [CommonModule],
+  imports: [CommonModule, TypeOrmModule.forFeature([User])],
 })
 export class AuthModule {}
