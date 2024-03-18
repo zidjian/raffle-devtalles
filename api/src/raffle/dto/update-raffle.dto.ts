563151cef0161e01b1045cbacf843f066c661bd3
Camilo <camilo.reyes_granada@uao.edu.co>
2024-03-18 07:58:07 -0500

    fix: is Optional status

--- api/src/raffle/dto/update-raffle.dto.ts
+++ api/src/raffle/dto/update-raffle.dto.ts
@@ -1,12 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRaffleDto } from './create-raffle.dto';
-import { IsArray, IsString, ValidateNested } from 'class-validator';
+import { IsArray, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { UpdatePrizeDto } from 'src/prize/dto/update-prize.dto';
 import { Type } from 'class-transformer';
 import { RaffleStatus } from '../entities/raffle.entity';
 
 export class UpdateRaffleDto extends PartialType(CreateRaffleDto) {
   @IsString()
+  @IsOptional()
   status?: RaffleStatus;
 
   @IsArray()
