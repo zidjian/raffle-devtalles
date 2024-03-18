ab0e2a4f9ae4a2e1e49442aa88f98b33c1648f63
Camilo <camilo.reyes_granada@uao.edu.co>
2024-03-17 23:37:43 -0500

    add: check guild end point

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -15,14 +15,6 @@ import { LoginUserDto } from './dto/login-user.dto';
 import { Auth } from './decorators/auth.decorator';
 import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
-/* import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from './decorators/get-user.decorator';
-import { User } from './entities/user.entity';
-import { RawHeaders } from './decorators/raw-header.decorator';
-import { UserRoleGuard } from './guards/user-role/user-role.guard';
-import { RoleProtected } from './decorators/role-protected/role-protected.decorator';
-import { ValidRoles } from './interfaces/valid-roles';
-import { Auth } from './decorators/auth.decorator'; */
 
 @Controller('auth')
 export class AuthController {
@@ -44,6 +36,14 @@ export class AuthController {
     return this.authService.register(createUserDto);
   }
 
+  @Get('/check-discordGuild')
+  @Auth()
+  checkDiscordGuild(@GetUser('id') userId: string) {
+    console.log('userId', userId);
+
+    return this.authService.checkDiscordGuild(userId);
+  }
+
   @Post('/login')
   login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
@@ -54,41 +54,4 @@ export class AuthController {
   checkAuthStatus(@GetUser() user: User) {
     return this.authService.checkAuthStatus(user);
   }
-
-  /*     @Get('private')
-  @UseGuards(AuthGuard()) 
-  testingPrivateRoute(
-    @GetUser() user: User,
-    @GetUser('email') userEmail: string,
-    @Req() request: Express.Request,
-
-    @RawHeaders() rawHeaders: string[],
-  ) {
-    return {
-      ok: true,
-      message: 'Hola mundo Private',
-      user,
-      userEmail,
-      rawHeaders,
-    };
-  } 
-
-  @Get('private2')
-  @RoleProtected()
-  @UseGuards(AuthGuard(), UserRoleGuard)
-  privateRoute2(@GetUser() user: User) {
-    return {
-      ok: true,
-      user,
-    };
-  }
-
-  @Get('private3')
-  @Auth(ValidRoles.admin)
-  privateRoute3(@GetUser() user: User) {
-    return {
-      ok: true,
-      user,
-    };
-  } */
 }
