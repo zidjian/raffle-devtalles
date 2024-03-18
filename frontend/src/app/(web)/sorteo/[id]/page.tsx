75a58ab156e0c82edf033c6147eca73b57c45d8c
Camilo <camilo.reyes_granada@uao.edu.co>
2024-03-18 09:52:13 -0500

    add: color

--- frontend/src/app/(web)/sorteo/[id]/page.tsx
+++ frontend/src/app/(web)/sorteo/[id]/page.tsx
@@ -1,111 +1,119 @@
-"use client";
+'use client';
 
-import { useSession } from "next-auth/react";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 interface Props {
-    params: {
-        id: string;
-    };
+  params: {
+    id: string;
+  };
 }
 
 export default function SorteoPage({ params }: Props) {
-    let today: string | Date = new Date();
-    today = today.toISOString().substring(0, 10);
-    const [raffle, setRaffle]: any = useState([]);
-    const { data: session }: any = useSession();
+  let today: string | Date = new Date();
+  today = today.toISOString().substring(0, 10);
+  const [raffle, setRaffle]: any = useState([]);
+  const { data: session }: any = useSession();
 
-    useEffect(() => {
-        fetch("http://localhost:3001/api/raffle/" + params.id)
-            .then((res) => res.json())
-            .then((res) => {
-                setRaffle(res);
+  useEffect(() => {
+    fetch('http://localhost:3001/api/raffle/' + params.id)
+      .then((res) => res.json())
+      .then((res) => {
+        setRaffle(res);
+      });
+  }, []);
+
+  function verified() {
+    if (session.user) {
+      const myHeaders = new Headers();
+      myHeaders.append(
+        'Authorization',
+        'Bearer ' + session.user.user.token.trim()
+      );
+
+      fetch('http://localhost:3001/api/raffle/participate/' + params.id, {
+        method: 'POST',
+        headers: myHeaders,
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          if (res.statusCode == 400) {
+            Swal.fire({
+              icon: 'error',
+              title: 'Alerta',
+              text: res.message,
+              timer: 30000,
+              showCancelButton: false,
+              showConfirmButton: true,
             });
-    }, []);
-
-    function verified() {
-        if (session.user) {
-            const myHeaders = new Headers();
-            myHeaders.append(
-                "Authorization",
-                "Bearer " + session.user.user.token.trim()
-            );
-
-            fetch("http://localhost:3001/api/raffle/participate/" + params.id, {
-                method: "POST",
-                headers: myHeaders,
-            })
-                .then((res) => res.json())
-                .then((res) => {
-                    if (res.statusCode == 400) {
-                        Swal.fire({
-                            icon: "error",
-                            title: "Alerta",
-                            text: res.message,
-                            timer: 30000,
-                            showCancelButton: false,
-                            showConfirmButton: true,
-                        });
-                        // goTo.push("/administrador");
-                    } else if (res.ok == true) {
-                        Swal.fire({
-                            icon: "success",
-                            title: "Gracias por participar",
-                            text: res.message,
-                            timer: 30000,
-                            showCancelButton: false,
-                            showConfirmButton: true,
-                        });
-                    }
-                });
-        }
+            // goTo.push("/administrador");
+          } else if (res.ok == true) {
+            Swal.fire({
+              icon: 'success',
+              title: 'Gracias por participar',
+              text: res.message,
+              timer: 30000,
+              showCancelButton: false,
+              showConfirmButton: true,
+            });
+          }
+        });
     }
+  }
 
-    return (
-        <>
-            <div className="py-4 px-4 lg:px-0">
-                <div className="w-full mx-auto md:max-w-7xl flex flex-col">
-                    <div className="flex flex-col">
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                            <div className="md:p-20 lg:p-32">
-                                <figure className="relative h-full min-h-64">
-                                    <Image
-                                        fill
-                                        src={
-                                            "https://ik.imagekit.io/noj6wnuqy/devtalles-raffle/wink.png?updatedAt=1710554265802"
-                                        }
-                                        alt={"Imagen del sorteo uno"}
-                                    />
-                                </figure>
-                            </div>
-                            <div className="flex flex-col justify-center items-start gap-4 md:py-24 lg:py-48">
-                                <h1 className="font-black text-5xl">
-                                    {raffle?.title}
-                                </h1>
-                                <div
-                                    className={`font-black text-xl ${
-                                        raffle?.deadLine >= today
-                                            ? "text-[#46ca58]"
-                                            : "text-[#e03939]"
-                                    }`}
-                                >
-                                    {raffle?.deadLine}
-                                </div>
-                                <p>{raffle?.description}</p>
+  return (
+    <>
+      <div className='py-4 px-4 lg:px-0'>
+        <div className='w-full mx-auto md:max-w-7xl flex flex-col'>
+          <div className='flex flex-col'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+              <div className='md:p-20 lg:p-32'>
+                <figure className='relative h-full min-h-64'>
+                  <Image
+                    fill
+                    src={
+                      'https://ik.imagekit.io/noj6wnuqy/devtalles-raffle/wink.png?updatedAt=1710554265802'
+                    }
+                    alt={'Imagen del sorteo uno'}
+                  />
+                </figure>
+              </div>
+              <div className='flex flex-col justify-center items-start gap-4 md:py-24 lg:py-48'>
+                <h1 className='font-black text-5xl'>{raffle?.title}</h1>
+                <div
+                  className={`font-black text-xl ${
+                    raffle?.deadLine >= today && raffle?.status === 'activo'
+                      ? 'text-[#46ca58]'
+                      : 'text-[#e03939]'
+                  }`}
+                >
+                  {raffle?.deadLine}
+                </div>
+                <div
+                  className={`font-black text-xl ${
+                    raffle?.deadLine >= today && raffle?.status === 'activo'
+                      ? 'text-[#46ca58]'
+                      : 'text-[#e03939]'
+                  }`}
+                >
+                  {raffle?.status}
+                </div>
 
-                                <div
-                                    onClick={() => verified()}
-                                    className="py-2 px-4 font-bold rounded-lg bg-[#E8D2FF] text-[#0F0A1E] hover:bg-white cursor-pointer"
-                                >
-                                    Participar
-                                </div>
-                            </div>
-                        </div>
-                        {/* <div className="py-12">
+                <p>{raffle?.description}</p>
+
+                <div
+                  onClick={() => verified()}
+                  className='py-2 px-4 font-bold rounded-lg bg-[#E8D2FF] text-[#0F0A1E] hover:bg-white cursor-pointer'
+                >
+                  Participar
+                </div>
+              </div>
+            </div>
+            {/* <div className="py-12">
                             <h2 className="font-black text-4xl text-center mb-12">
                                 Premios
                             </h2>
@@ -136,9 +144,9 @@ export default function SorteoPage({ params }: Props) {
                                 </div>
                             </div>
                         </div> */}
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- frontend/src/app/(web)/sorteos/page.tsx
+++ frontend/src/app/(web)/sorteos/page.tsx
@@ -1,63 +1,98 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/formElement";
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import { Button } from '@/components/formElement';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function RafflePage() {
-    let today: string | Date = new Date();
-    today = today.toISOString().substring(0, 10);
-    const [raffles, setRaffles] = useState([]);
+  let today: string | Date = new Date();
+  today = today.toISOString().substring(0, 10);
+  const [raffles, setRaffles] = useState([]);
 
-    useEffect(() => {
-        fetch("http://localhost:3001/api/raffle")
-            .then((res) => res.json())
-            .then((res) => {
-                setRaffles(res);
-            });
-    }, []);
+  useEffect(() => {
+    fetch('http://localhost:3001/api/raffle')
+      .then((res) => res.json())
+      .then((res) => {
+        setRaffles(res);
+      });
+  }, []);
 
-    function verified() {}
+  function verified() {}
 
-    return (
-        <>
-            <div className="py-4 px-2 lg:px-0">
-                <div className="w-full mx-auto md:max-w-7xl p-8 md:p-12 lg:p-24 flex flex-col">
-                    <h1 className="font-black text-3xl text-center mb-12">
-                        Nuetros sorteos
-                    </h1>
-                    <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
-                        {raffles.map((raffle: any) => (
-                            <Link
-                                key={raffle?.title}
-                                href={"/sorteo/" + raffle?.id}
-                                className="flex flex-col gap-4"
-                            >
-                                <figure className="relative h-80 border-4 border-[#000] rounded-xl">
-                                    <div
-                                        className={`py-2 px-4 font-bold rounded-lg  ${
-                                            raffle?.deadLine >= today
-                                                ? "bg-[#46ca58]"
-                                                : "bg-[#e03939]"
-                                        } text-[#0F0A1E] border-4 border-[#000] absolute -top-6 left-1/2 translate-x-[-50%]`}
-                                    >
-                                        {raffle?.deadLine}
-                                    </div>
-                                    <Image
-                                        fill
-                                        src={
-                                            "https://ik.imagekit.io/noj6wnuqy/devtalles-raffle/wink.png"
-                                        }
-                                        alt={"Imagen del sorteo"}
-                                    />
-                                </figure>
-                                <h2 className="font-bold text-xl text-center">
-                                    {raffle?.title}
-                                </h2>
-                            </Link>
-                        ))}
-                        {/* <Link
+  return (
+    <>
+      <div className='py-4 px-2 lg:px-0'>
+        <div className='w-full mx-auto md:max-w-7xl p-8 md:p-12 lg:p-24 flex flex-col'>
+          <h1 className='font-black text-3xl text-center mb-12'>
+            Nuetros sorteos
+          </h1>
+          <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16'>
+            {raffles.map((raffle: any) => {
+              if (raffle?.status === 'finalizado') {
+                return (
+                  <Link
+                    key={raffle?.title}
+                    href={'/sorteo/' + raffle?.id}
+                    className='flex flex-col gap-4'
+                  >
+                    <figure className='relative h-80 border-4 border-[#000] rounded-xl'>
+                      <div
+                        className={`py-2 px-4 font-bold rounded-lg  ${
+                          raffle?.deadLine >= today &&
+                          raffle?.status === 'activo'
+                            ? 'bg-[#46ca58]'
+                            : 'bg-[#e03939]'
+                        } text-[#0F0A1E] border-4 border-[#000] absolute -top-6 left-1/2 translate-x-[-50%]`}
+                      >
+                        {raffle?.deadLine}
+                      </div>
+                      <Image
+                        fill
+                        src={
+                          'https://ik.imagekit.io/noj6wnuqy/devtalles-raffle/wink.png'
+                        }
+                        alt={'Imagen del sorteo'}
+                      />
+                    </figure>
+                    <h2 className='font-bold text-xl text-center'>
+                      {raffle?.title}
+                    </h2>
+                  </Link>
+                );
+              } else
+                return (
+                  <Link
+                    key={raffle?.title}
+                    href={'/sorteo/' + raffle?.id}
+                    className='flex flex-col gap-4'
+                  >
+                    <figure className='relative h-80 border-4 border-[#000] rounded-xl'>
+                      <div
+                        className={`py-2 px-4 font-bold rounded-lg  ${
+                          raffle?.deadLine >= today &&
+                          raffle?.status === 'activo'
+                            ? 'bg-[#46ca58]'
+                            : 'bg-[#e03939]'
+                        } text-[#0F0A1E] border-4 border-[#000] absolute -top-6 left-1/2 translate-x-[-50%]`}
+                      >
+                        {raffle?.deadLine}
+                      </div>
+                      <Image
+                        fill
+                        src={
+                          'https://ik.imagekit.io/noj6wnuqy/devtalles-raffle/wink.png'
+                        }
+                        alt={'Imagen del sorteo'}
+                      />
+                    </figure>
+                    <h2 className='font-bold text-xl text-center'>
+                      {raffle?.title}
+                    </h2>
+                  </Link>
+                );
+            })}
+            {/* <Link
                             href={"/sorteo/123"}
                             className="flex flex-col gap-4"
                         >
@@ -85,9 +120,9 @@ export default function RafflePage() {
                                 </div>
                             </div>
                         </Link> */}
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
