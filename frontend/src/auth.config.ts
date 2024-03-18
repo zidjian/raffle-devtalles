6dd19d45793d0552448cbeba8cd3953bc934348c
Camilo <camilo.reyes_granada@uao.edu.co>
2024-03-18 08:36:54 -0500

    add: Login with user and password

--- frontend/src/auth.config.ts
+++ frontend/src/auth.config.ts
@@ -1,73 +1,114 @@
-import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import DiscordProvider from "next-auth/providers/discord";
-import type { NextAuthConfig } from "next-auth";
-import { z } from "zod";
-import bcrypt from "bcrypt";
-import { redirect } from "next/navigation";
-import { json } from "stream/consumers";
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import DiscordProvider from 'next-auth/providers/discord';
+import type { NextAuthConfig } from 'next-auth';
+import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import { redirect } from 'next/navigation';
+import { json } from 'stream/consumers';
 
 export const authConfig: NextAuthConfig = {
-    trustHost: true,
-    pages: {
-        signIn: "/iniciar-sesion",
+  trustHost: true,
+  pages: {
+    signIn: '/iniciar-sesion',
+  },
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
     },
 
-    callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-            return true;
-        },
+    async jwt({ token, user, account, profile }) {
+      if (account?.provider == 'credentials') {
+        token.data = {
+          user: {
+            ...user,
+          },
+        };
 
-        async jwt({ token, user, account, profile }) {
-            if (account && profile) {
-                const data = {
-                    email: user.email || "",
-                    discordId: profile.id || "",
-                    username: user.name || "",
-                    access_token: account?.access_token || "",
-                    refresh_token: account?.refresh_token || "",
-                    avatar: user.image || "",
-                };
+        return token;
+      } else if (account?.provider == 'discord') {
+        const data = {
+          email: user.email || '',
+          discordId: profile.id || '',
+          username: user.name || '',
+          access_token: account?.access_token || '',
+          refresh_token: account?.refresh_token || '',
+          avatar: user.image || '',
+        };
 
-                const myHeaders = new Headers();
-                myHeaders.append("Content-Type", "application/json");
+        const myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/json');
 
-                let extend = await fetch(
-                    "http://localhost:3001/api/auth/register-discord",
-                    {
-                        method: "POST",
-                        headers: myHeaders,
-                        body: JSON.stringify(data),
-                    }
-                );
-                let extendData = await extend.json();
+        let extend = await fetch(
+          'http://localhost:3001/api/auth/register-discord',
+          {
+            method: 'POST',
+            headers: myHeaders,
+            body: JSON.stringify(data),
+          }
+        );
+        let extendData = await extend.json();
 
-                token.data = {
-                    user: {
-                        ...user,
-                        token: extendData.token,
-                        roles: extendData.roles,
-                    },
-                };
-            }
+        token.data = {
+          user: {
+            ...user,
+            token: extendData.token,
+            roles: extendData.roles,
+          },
+        };
+      }
 
-            return token;
-        },
-
-        session({ session, token, user }) {
-            session.user = token.data as any;
-
-            return session;
-        },
+      return token;
     },
-    providers: [
-        DiscordProvider({
-            clientId: process.env.DISCORD_CLIENT_ID,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET,
-            authorization:
-                "https://discord.com/api/oauth2/authorize?scope=identify+email+guilds+guilds.members.read",
-        }),
-    ],
+
+    session({ session, token, user }) {
+      session.user = token.data as any;
+
+      return session;
+    },
+  },
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization:
+        'https://discord.com/api/oauth2/authorize?scope=identify+email+guilds+guilds.members.read',
+    }),
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'Login',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        // Realizar la solicitud de inicio de sesión al backend con correo y contraseña
+
+        const response = await fetch('http://localhost:3001/api/auth/login', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            email: credentials.email,
+            password: credentials.password,
+          }),
+        });
+
+        if (response.ok) {
+          // Extraer el token JWT de la respuesta
+          const data = await response.json();
+
+          // Devolver el token JWT
+          return data;
+        } else {
+          // Manejar errores de inicio de sesión
+          const error = await response.text();
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
 };
 
 export const { signIn, signOut, auth, handlers } = NextAuth(authConfig);
--- frontend/src/components/form/LoginForm.tsx
+++ frontend/src/components/form/LoginForm.tsx
@@ -1,69 +1,57 @@
-"use client";
+'use client';
 
-import { Button, InputText } from "../formElement";
-import * as yup from "yup";
-import { useFormik } from "formik";
+import { Button, InputText } from '../formElement';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 
-import Link from "next/link";
+import Link from 'next/link';
+import { signIn } from 'next-auth/react';
 
 export function LoginForm() {
-    let schema = yup.object({
-        email: yup.string().email().required("Campo obligatorio"),
-        password: yup.string().required("Campo obligatorio"),
-    });
+  let schema = yup.object({
+    email: yup.string().email().required('Campo obligatorio'),
+    password: yup.string().required('Campo obligatorio'),
+  });
 
-    const formik = useFormik({
-        initialValues: {
-            email: "",
-            password: "",
-        },
-        enableReinitialize: true,
-        validationSchema: schema,
-        onSubmit: async (values) => {
-            console.log("Iniciar sesión");
-        },
-    });
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    enableReinitialize: true,
+    validationSchema: schema,
+    onSubmit: async (values) => {
+      signIn('credentials', { ...values });
+    },
+  });
 
-    return (
-        <form
-            onSubmit={formik.handleSubmit}
-            className="flex flex-col gap-4 w-full"
-        >
-            <InputText
-                name={"email"}
-                label={"Email"}
-                placeholder="ejemplo@gmail.com"
-                value={formik.values.email}
-                change={formik.handleChange}
-                blur={formik.handleBlur}
-                error={formik.touched.email && Boolean(formik.errors.email)}
-                helpertext={formik.touched.email && formik.errors.email}
-            />
-            <InputText
-                name={"password"}
-                label={"Contrasseña"}
-                type="password"
-                placeholder="********"
-                value={formik.values.password}
-                change={formik.handleChange}
-                blur={formik.handleBlur}
-                error={
-                    formik.touched.password && Boolean(formik.errors.password)
-                }
-                helpertext={formik.touched.password && formik.errors.password}
-            />
-            <p className="text-sm">
-                ¿No estas registrado? Registrate{" "}
-                <Link
-                    href={"/registro"}
-                    className="underline underline-offset-2"
-                >
-                    aquí
-                </Link>
-            </p>
-            <div className="w-full text-center">
-                <Button text="Iniciar sesión" type="submit" />
-            </div>
-        </form>
-    );
+  return (
+    <form onSubmit={formik.handleSubmit} className='flex flex-col gap-4 w-full'>
+      <InputText
+        name={'email'}
+        label={'Email'}
+        placeholder='ejemplo@gmail.com'
+        value={formik.values.email}
+        change={formik.handleChange}
+        blur={formik.handleBlur}
+        error={formik.touched.email && Boolean(formik.errors.email)}
+        helpertext={formik.touched.email && formik.errors.email}
+      />
+      <InputText
+        name={'password'}
+        label={'Contrasseña'}
+        type='password'
+        placeholder='********'
+        value={formik.values.password}
+        change={formik.handleChange}
+        blur={formik.handleBlur}
+        error={formik.touched.password && Boolean(formik.errors.password)}
+        helpertext={formik.touched.password && formik.errors.password}
+      />
+
+      <div className='w-full text-center'>
+        <Button text='Iniciar sesión' type='submit' />
+      </div>
+    </form>
+  );
 }
